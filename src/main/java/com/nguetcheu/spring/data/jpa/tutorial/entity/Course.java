ca6c0ab80a991863a681a5e6c03b10f5df1d5a9c
nguetcheu <dnguetcheu@gmail.com>
2023-07-18 15:42:39 +0200

    Relation Many to many implémentation ok

--- src/main/java/com/nguetcheu/spring/data/jpa/tutorial/entity/Course.java
+++ src/main/java/com/nguetcheu/spring/data/jpa/tutorial/entity/Course.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -45,4 +47,26 @@ public class Course {
     )
     private Teacher teacher;
 
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "student_course_map",
+            joinColumns = @JoinColumn(
+                    name = "course_id",
+                    referencedColumnName = "courseId"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId"
+            )
+    )
+    private List<Student> students;
+
+    public void addStudents(Student student){
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(student);
+    }
 }
--- src/test/java/com/nguetcheu/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/com/nguetcheu/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.nguetcheu.spring.data.jpa.tutorial.repository;
 
 import com.nguetcheu.spring.data.jpa.tutorial.entity.Course;
+import com.nguetcheu.spring.data.jpa.tutorial.entity.Student;
 import com.nguetcheu.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -37,6 +41,91 @@ class CourseRepositoryTest {
                 .credit(6)
                 .teacher(teacher)
                 .build();
+
+        courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination(){
+        Pageable fistPageWithThreeRecords =
+                PageRequest.of(0,3);
+
+        Pageable secondPageWithTwoRecords =
+        PageRequest.of(1, 2);
+
+        List<Course> courses =
+                courseRepository.findAll(secondPageWithTwoRecords)
+                        .getContent();
+
+        Long totalElements =
+                courseRepository.findAll(secondPageWithTwoRecords)
+                                .getTotalElements();
+
+        Long totalPages =
+                (long) courseRepository.findAll(secondPageWithTwoRecords)
+                        .getTotalPages();
+
+        System.out.println("totalPages = " + totalPages);
+
+        System.out.println("totalElements = " + totalElements);
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void findAllSorting() {
+        Pageable sortByTitle =
+                PageRequest.of(0,2, Sort.by("title"));
+
+        Pageable sortByCreditDesc =
+                PageRequest.of(0,2, Sort.by("credit").descending());
+
+        Pageable sortByTitleAndCreditDesc =
+                PageRequest.of(0,2, Sort.by("title").descending().and(Sort.by("credit"))
+                );
+
+        List<Course> courses =
+                courseRepository.findAll(sortByTitle)
+                        .getContent();
+
+        System.out.println("courses = " + courses);
+
+    }
+
+    @Test
+    public void printFindByTitleContaining(){
+        Pageable firstPageTenRecords =
+                PageRequest.of(0, 10);
+
+        List<Course> courses =
+                courseRepository.findByTitleContaining(
+                        "D", firstPageTenRecords
+                ).getContent();
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+        Teacher teacher = Teacher.builder()
+                .firstName("Lizze")
+                .lastName("Morgan")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Abhisheck")
+                .lastName("Singh")
+                .emailId("abhisheck@gmail.com")
+                .build();
+
+        Course course = Course
+                .builder()
+                .title("AI")
+                .credit(12)
+                .teacher(teacher)
+                .build();
+
+        course.addStudents(student);
 
         courseRepository.save(course);
     }
